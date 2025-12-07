4dff0f9022523b38c1d67798c4e748c415218994
Jacky Chan <rumbleman3@hotmail.com>
2025-12-07 12:14:23 -0800

    Day 1 completed brute force

--- day1.py
+++ day1.py
@@ -20,21 +20,37 @@ for code in codes:
 
 print(password)
 
+# Gold star Brute force
+dial = 50
+password = 0
+for code in codes:
+    if code[0] == 'L':
+        for i in range(int(code[1:])):
+            dial = dial - 1
+            if dial == 0:
+                password += 1
+            elif dial == -1:
+                dial = 99 
+    else:
+        for i in range(int(code[1:])):
+            dial = dial + 1
+            if dial == 100:
+                password += 1
+                dial = 0
+print(password)
+
 # Gold star
 dial = 50
 password = 0
 for code in codes:
     if code[0] == 'L':
-        if (dial < int(code[1:])):
-            if (dial == 0):
-                password -= 1
-            password += math.floor(abs(dial - int(code[1:])) / 100) + 1
-        dial = (dial - int(code[1:])) % 100      
+        if (dial <= int(code[1:])):
+            if (dial != 0 or int(code[1:]) >= 100):
+                password += math.floor(abs(dial - int(code[1:])) / 100) + 1
+        dial = (dial - int(code[1:])) % 100    
     else:
-        if (dial + int(code[1:]) > 100):
+        if (dial + int(code[1:]) >= 100):
             password += math.floor(abs(dial + int(code[1:])) / 100)
         dial = (dial + int(code[1:])) % 100
-    if dial == 0:
-        password += 1
 
 print(password)
